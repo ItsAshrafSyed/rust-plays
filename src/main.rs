b74788c348446e7a5985d29636133d2cb2138ddc
ItsAshrafSyed <ashrafsyed0103@gmail.com>
2025-09-22 02:05:27 +0530

    more playback

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Hello, world!");
+
     ///////////////// primitive types in rust
     // integer, float, boolean, char
     // integer: i8, i16, i32, i64, i128
@@ -16,6 +17,7 @@ fn main() {
         "i32: {}, u32: {}, f64: {}, bool: {}, char: {}",
         i32_var, u32_var, f64_var, bool_var, char_var
     );
+
     ///////////////// compound types in rust
     // tuple, array, slices ,strings (slice string)
     // Array
@@ -34,7 +36,7 @@ fn main() {
     let fruit_slice: &[&str] = &["mango", "orange", "grape"];
     println!("Fruit slice: {:?}", fruit_slice);
 
-    // String and &str (slice string)
+    ///////////////////// String and &str (slice string)
     // String: growable, mutable, owned, allocated on heap. (any data type by default is immutable)
     let mut string_var: String = String::from("Hello, Rust!"); // String::from is used to create a String from a string literal as "Hello, Rust!" is of type &str and put it on heap memory it is equivalent to let string_var = "Hello, Rust!".to_string();
     println!("Computer says {}", string_var);
@@ -48,49 +50,40 @@ fn main() {
     let str_slice2: &str = &str_slice[7..11];
     println!("String slice of selected indexs: {}", str_slice2); // slicing the string slice to get "Rust"
 
-    let numbers: [i32; 3] = [1, 2, 3];
-    let strings: [&str; 3] = ["one", "two", "three"];
-    let floats: [f32; 3] = [1.1, 2.2, 3.3];
-    let bools: [bool; 3] = [true, false, true];
-    let chars: [char; 3] = ['a', 'b', 'c'];
-    let tuples: [(i32, &str); 3] = [(1, "one"), (2, "two"), (3, "three")];
-    let arrays: [[i32; 3]; 3] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    let books: &[String; 3] = &["one".to_string(), "two".to_string(), "three".to_string()];
-    let cold: String = "cold".to_string();
+    ///////////////// ownership, borrowing in rust
+    //// ownership:
+    // 1.each value in Rust has a variable that’s called its owner.
+    // 2.there can only be one owner at a time.
+    // 3.when the owner goes out of scope, the value will be dropped.
+    //// borrowing & references:
+    // 1.references allow you to refer to some value without taking ownership of it.
+    // 2.you can have either one mutable reference or any number of immutable references to a value.
 
+    // /////////////// struct and impl in rust
     let mut account = BankAccout {
-        owner: "John".to_string(),
-        balance: 100.0,
+        owner: String::from("Jhon"),
+        balance: 100.00,
     };
-
     account.check_balance();
-    account.withdraw(50.0);
+    account.withdraw(20.0);
     account.check_balance();
 
-    //struct
+    // - evary variable by default is immutable in rust unless we use mut keyword
+    // - constants are always immutable in rust meaning we cannot use mut keyword with constants, must declare the type of constant & use all uppercase with underscores. you can declare constants in any scope including global scope
+    // const MAX_POINTS: u32 = 100_000;
+}
 
-    struct BankAccout {
-        owner: String,
-        balance: f32,
+struct BankAccout {
+    owner: String,
+    balance: f32,
+}
+impl BankAccout {
+    fn withdraw(&mut self, amount: f32) {
+        self.balance -= amount;
+        println!("Withdrew: {}", amount);
     }
 
-    impl BankAccout {
-        fn withdraw(&mut self, amount: f32) {
-            self.balance -= amount;
-            println!("Withdrew: {}", amount);
-        }
-
-        fn check_balance(&self) {
-            println!("Balance: {}", self.balance);
-        }
+    fn check_balance(&self) {
+        println!("Owner: {}, Balance: {}", self.owner, self.balance);
     }
-
-    println!("{:?}", numbers);
-    println!("{:?}", strings);
-    println!("{:?}", floats);
-    println!("{:?}", bools);
-    println!("{:?}", chars);
-    println!("{:?}", tuples);
-    println!("{:?}", arrays);
-    println!("{:?}", books);
 }
